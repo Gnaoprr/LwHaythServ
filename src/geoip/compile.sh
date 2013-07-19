2d9a602dc030000f388802a17a776e3ea7b2088d
Gnaoprr <bryan.morabito@newsocialife.com>
2013-07-19 05:34:41 +0000

    Fixes #1

--- src/geoip/compile.sh
+++ src/geoip/compile.sh
@@ -1,2 +1,4 @@
 gcc -Wall -fPIC -c main.c -o geoip.o
 gcc -shared geoip.o -lGeoIP -o libgeoip.so
+rm geoip.o
+mv libgeoip.so ../../bin/modules
--- src/geoip/main.c
+++ src/geoip/main.c
@@ -3,12 +3,47 @@
 #include "GeoIP.h"
 #include "GeoIPCity.h"
 
+static GeoIP * geoip = NULL;
+static GeoIP * geocity = NULL;
+
+char geodb[260] = "GeoIP.dat";
+char geocitydb[260] = "GeoLiteCity.dat";
+
+void load_geoip_database() {
+    if(geoip) GeoIP_delete(geoip);
+    geoip = GeoIP_open(geodb, GEOIP_STANDARD | GEOIP_MEMORY_CACHE);
+}
+
+void load_geocity_database() {
+    if(geocity) GeoIP_delete(geocity);
+    geocity = GeoIP_open(geocitydb, GEOIP_STANDARD);
+}
+
+const char *ip_to_country(const char *ip) {
+    if(!geoip) return "Unknown";
+    const char *_country = GeoIP_country_name_by_addr(geoip, ip);
+    const char *country = _country ? _country : "Unknown";
+    return country;
+}
+
+const char *ip_to_continent(const char *ip) {
+    if(!geoip) return "Unknown";
+    const char *_continent = GeoIP_continent_by_id(GeoIP_country_id_by_addr(geoip, ip));
+    const char *continent = (_continent && _continent != "--") ? _continent : "Unknown";
+    return continent;
+}
+
+const char *ip_to_city(const char *ip) {
+    if(!geocity) return "Unknown";
+    GeoIPRecord *r = GeoIP_record_by_addr(geocity, ip);
+    const char *city = (r && r->city) ? r->city : "Unknown";
+    return city;
+}
+
 enum { PRIV_NONE = 0, PRIV_MASTER, PRIV_AUTH, PRIV_ADMIN, PRIV_ROOT };
 
 void *(* z_getext)(char *);
 void (* z_setext)(char *, void *);
-
-char dbfile[260] = "GeoIP.dat";
 
 struct hookparam
 {
@@ -56,16 +91,11 @@ int on_connect(struct hookparam *hp)
     
     sprintf(addr, "%i.%i.%i.%i", ip&0xFF, (ip>>8)&0xFF, (ip>>16)&0xFF, (ip>>24)&0xFF);
     
-    char *country = GeoIP_country_name_by_addr(gi, addr);
-    char *continent = GeoIP_continent_by_id(GeoIP_country_id_by_addr(gi, addr));
+    sprintf(msg, "\f3>>> \f1%s \f2is fragging in \f1%s\f4, \f1%s\f4, \f1%s\f4. (\f7IP\f4-\f7Address\f4: \f5%s\f4)", name, ip_to_city(addr), ip_to_country(addr), ip_to_continent(addr), addr);
+    notifypriv(msg, PRIV_ADMIN, PRIV_ROOT);
     
-    sprintf(msg, "\f3>>> \f1%s \f2is fragging in \f1%s\f4, \f1%s\f4. (\f7IP\f4-\f7Address\f4: \f5%s\f4)", name, country?:"Unknown", continent?:"Unknown", addr);
-    notifypriv(msg, PRIV_AUTH, PRIV_ROOT);
-    
-    if(!country) return 0;
-    
-    sprintf(msg, "\f3>>> \f1%s \f2is fragging in \f1%s\f4, \f1%s\f4.", name, country?:addr, continent?:"Unknown");
-    notifypriv(msg, PRIV_NONE, PRIV_MASTER);
+    sprintf(msg, "\f3>>> \f1%s \f2is fragging in \f1%s\f4, \f1%s\f4, \f1%s\f4.", name, ip_to_city(addr), ip_to_country(addr), ip_to_continent(addr));
+    notifypriv(msg, PRIV_NONE, PRIV_AUTH);
     
     return 0;
 }
@@ -86,9 +116,11 @@ char *z_init(void *getext, void *setext, char *args)
         switch(argv[i][0])
         {
             case 'f':
-                strncpy(dbfile, argv[++i], 260);
+                strncpy(geodb, argv[++i], 260);
                 break;
-            
+            case 'F':
+                strncpy(geocitydb, argv[++i], 260);
+
             default:
                 return "Unknown switch";
         }
@@ -98,8 +130,8 @@ char *z_init(void *getext, void *setext, char *args)
     *(void **)(&sendf) = z_getext("sendf");
     *(void **)(&notifypriv) = z_getext("notifypriv");
     
-    gi = GeoIP_open(dbfile, GEOIP_STANDARD | GEOIP_MEMORY_CACHE);
-    if(!gi) return "Failed loading geoip database";
+    load_geoip_database();
+    load_geocity_database();
     
     *(void **)(&addhook) = z_getext("addhook");
     
